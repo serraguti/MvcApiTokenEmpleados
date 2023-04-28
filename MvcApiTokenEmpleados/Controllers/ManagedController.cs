8cb6b927a4b194ff0e871e70969766bf329c87ba
serraguti <pacoserranox@gmail.com>
2023-04-28 11:34:58 +0200

    commit

---
+++ MvcApiTokenEmpleados/Controllers/ManagedController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using MvcApiTokenEmpleados.Services;
+using System.Security.Claims;
+
+namespace MvcApiTokenEmpleados.Controllers
+{
+    public class ManagedController : Controller
+    {
+        private ServiceApiEmpleados service;
+
+        public ManagedController(ServiceApiEmpleados service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login
+            (string username, string password)
+        {
+            string token =
+                await this.service.GetTokenAsync(username, password);
+            if (token == null)
+            {
+                ViewData["MENSAJE"] = "Usuario/Password incorrectos";
+                return View();
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Bienvenid@ a mi App!!!";
+                HttpContext.Session.SetString("TOKEN", token);
+                ClaimsIdentity identity =
+                    new ClaimsIdentity
+                    (CookieAuthenticationDefaults.AuthenticationScheme,
+                    ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.Name, username));
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, password));
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync
+                    (CookieAuthenticationDefaults.AuthenticationScheme
+                    , userPrincipal, new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.UtcNow.AddMinutes(30)
+                    });
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync
+                (CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.Session.Remove("TOKEN");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- MvcApiTokenEmpleados/Program.cs
+++ MvcApiTokenEmpleados/Program.cs
@@ -1,7 +1,27 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using MvcApiTokenEmpleados.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ServiceApiEmpleados>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme =
+    CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme =
+    CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddCookie();
+
+builder.Services
+    .AddControllersWithViews(options => options.EnableEndpointRouting = false);
 
 var app = builder.Build();
 
@@ -18,10 +38,15 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseSession();
+app.UseMvc(routes =>
+{
+    routes.MapRoute(
+        name: "default",
+        template: "{controller=Home}/{action=Index}/{id?}"
+        );
+});
 
 app.Run();
